8c87c0048e1b4ccdbe5b4196afb9b5915adf52db
sanchuanhehe <2947512113@qq.com>
2025-03-09 01:04:49 +0800

    在mk_parser.h中添加mk_target结构体及其相关注释，以增强Makefile解析功能

--- include/mk_parser.h
+++ include/mk_parser.h
@@ -1,6 +1,22 @@
 #ifndef MK_PARSER_H
 #define MK_PARSER_H
 
+/**
+ * @brief make 目标结构体
+ */
+struct mk_target
+{
+    char *name;       //< 目标名称
+    char **deps;   //< 依赖目标
+    char **commands; //< 执行命令
+};
+
+/**
+ * @brief 解析Makefile文件
+ * 
+ * @param arg Makefile文件路径
+ * @return int 
+ */
 int mk_parser(const char *arg);
 
 #endif
